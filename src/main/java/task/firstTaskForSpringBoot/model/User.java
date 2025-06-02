0a97048ad816d58fe839d311ccb8e1fa41923861
RodUman1985 <Vand_85@mail.ru>
2025-06-02 16:27:59 +0300

    приложение с использованием DAO

--- src/main/java/task/firstTaskForSpringBoot/model/User.java
+++ src/main/java/task/firstTaskForSpringBoot/model/User.java
@@ -9,10 +9,7 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
-@ToString
-@AllArgsConstructor
+@Data
 @NoArgsConstructor
 public class User {
     @Id
@@ -35,19 +32,5 @@ public class User {
     @Email(message = "Email should be valid")
     private String email;
 
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        User user = (User) o;
-        return getId() != null && Objects.equals(getId(), user.getId());
-    }
 
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
 }
